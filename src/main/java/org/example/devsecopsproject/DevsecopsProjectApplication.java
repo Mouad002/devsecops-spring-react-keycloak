d1e570704c38c0cd689969317e222026b03b1ab6
mouad002 <elcosto02@gmail.com>
2026-01-03 17:03:48 +0100

    initialize project and the necessary microservices

---
+++ src/main/java/org/example/devsecopsproject/DevsecopsProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.devsecopsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsecopsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/devsecopsproject/DevsecopsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devsecopsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsecopsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
